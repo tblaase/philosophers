942d4521c983acca14c25d029aac5e506c211802
tblaase <tblaase@student.42.fr>
2022-01-03 21:21:26 +0100

    added death routine

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 16:05:06 by tblaase           #+#    #+#             */
-/*   Updated: 2021/12/28 18:01:57 by tblaase          ###   ########.fr       */
+/*   Updated: 2022/01/03 20:30:54 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,14 +47,26 @@ void	free_philos(void)
 	philos = NULL;
 }
 
-int	destroy_philo(t_philo *philo) // maybe not needed
-{
-	(void)philo;
-	return (EXIT_SUCCESS);
-}
-
 int	death_routine(t_philo *philo)
 {
-	(void)philo;
+	t_input	*input;
+	t_philo	**philos;
+	int		i;
+
+	i = 0;
+	input = get_input();
+	philos = get_philos();
+	input->death = true;
+	printf("%d %s", philo->philo_n, input->state[is_dead]);
+	if (destroy_forks() == EXIT_FAILURE)
+	{
+		while (philos[i] != NULL)
+			pthread_join(philos[i++]->thread_id, NULL);
+		free_philos();
+		return (EXIT_FAILURE);
+	}
+	while (philos[i] != NULL)
+		pthread_join(philos[i++]->thread_id, NULL);
+	free_philos();
 	return (EXIT_SUCCESS);
 }
